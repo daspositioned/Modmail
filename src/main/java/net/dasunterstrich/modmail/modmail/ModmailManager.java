f0f752aa493f85c5cba84ad856556f0da33fabc2
Daniel Scherf <munita.gavai@gmail.com>
2023-04-24 00:31:12 +0200

    Update debug message

--- src/main/java/net/dasunterstrich/modmail/modmail/ModmailManager.java
+++ src/main/java/net/dasunterstrich/modmail/modmail/ModmailManager.java
@@ -49,7 +49,7 @@ public class ModmailManager {
         var jda = user.getJDA();
         var guild = jda.getGuildById(config.get("GUILD_ID"));
         var forumChannel = guild.getForumChannelById(config.get("FORUM_ID"));
-        forumChannel.retrieveArchivedPublicThreadChannels().queue(retrievedThreads -> logger.info("Retrieved " + retrievedThreads.size()));
+        forumChannel.retrieveArchivedPublicThreadChannels().queue(retrievedThreads -> logger.info("Max limit: " + forumChannel.retrieveArchivedPublicThreadChannels().getMaxLimit()));
 
         try {
             var modmailThreadID = getModmailThread(user);
