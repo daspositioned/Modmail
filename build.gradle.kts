db4d7dcacbe8a0ba4cfd2402cbe64f8c7e2fb4a2
Daniel Scherf <munita.gavai@gmail.com>
2023-06-21 15:27:28 +0200

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,10 @@ repositories {
 dependencies {
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("com.impossibl.pgjdbc-ng:pgjdbc-ng:0.8.9")
-    implementation("ch.qos.logback:logback-classic:1.4.6")
+    implementation("ch.qos.logback:logback-classic:1.4.7")
     implementation("io.github.cdimascio:dotenv-java:2.3.2")
 
-    implementation("net.dv8tion:JDA:5.0.0-beta.10") {
+    implementation("net.dv8tion:JDA:5.0.0-beta.11") {
         exclude("opus-java")
     }
 }
