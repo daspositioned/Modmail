936ff6f64ceeea7ec974160bfab2f1466d8d5d71
Daniel Scherf <munita.gavai@gmail.com>
2023-06-24 13:39:49 +0200

    Switch from global display name to unique global name

--- src/main/java/net/dasunterstrich/modmail/utils/UsernameUtils.java
+++ src/main/java/net/dasunterstrich/modmail/utils/UsernameUtils.java
@@ -4,11 +4,10 @@ import net.dv8tion.jda.api.entities.User;
 
 public class UsernameUtils {
     public static String getUsername(User user) {
-        var name = user.getGlobalName();
-        if (name == null) {
-            name = user.getAsTag();
+        if (user.getGlobalName() == null) {
+            return user.getAsTag();
         }
 
-        return name;
+        return user.getName();
     }
 }
